c5b26c7f5d05bc8931fe906cc2253c96933b6646
benni <benni@posteo.de>
2012-08-28 16:33:09 +0200

    Get also separator from I18n locale file.

--- lib/localize_input.rb
+++ lib/localize_input.rb
@@ -5,18 +5,20 @@ module LocalizeInput
   end
 
   module ClassMethods
-    # Use this in you ActiveRecord class to replace the local seperator with database format
+    # Use this in your ActiveRecord class to replace the local separator with database format
     # e.g.: localize_input_of :price
     def localize_input_of(*attr_names)
       attr_names.flatten.each do |attr|
         define_method "#{attr}=" do |input|
           begin
-            seperator = I18n.t("separator", :scope => "number.format")
+            Rails.logger.debug "Input: #{input.inspect}"
+            separator = I18n.t("separator", :scope => "number.format")
             delimiter = I18n.t("delimiter", :scope => "number.format")
-            input.gsub!(delimiter, "") if input.match(/\d+#{Regexp.escape(delimiter)}+\d+#{Regexp.escape(seperator)}+\d+/) # Remove delimiter
-            input.gsub!(seperator, ".") # Replace seperator with db compatible character
+            input.gsub!(delimiter, "") if input.match(/\d+#{Regexp.escape(delimiter)}+\d+#{Regexp.escape(separator)}+\d+/) # Remove delimiter
+            input.gsub!(separator, ".") # Replace separator with db compatible character
             self[attr] = input
           rescue
+            Rails.logger.warn "Can't localize input: #{input}"
             self[attr] = input
           end
         end
